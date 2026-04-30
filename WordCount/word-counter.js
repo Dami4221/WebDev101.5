9939ab548e264281d6a4902032007ddbbed53f16
Dami <pamiendeal@gmail.com>
2026-04-30 10:46:00 -0500

    Start Word count

---
+++ WordCount/word-counter.js
@@ -0,0 +1,6 @@
+let $P = document.querySelector("p");
+let $TEXT = $P.innerText;
+let $LIST = $TEXT.split(" ");
+let $WORDS = $LIST.length;
+let $TIME = $WORDS / 250;
+$P.innerText += " (Reading Time: " + $TIME + " minutes)";
