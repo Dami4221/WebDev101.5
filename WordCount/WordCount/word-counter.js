a28965bfe7d3e3e8e98787d45d6e539490e442c3
Demi Peal <pamiendeal@gmail.com>
2026-05-13 10:39:30 -0500

    functional complete

--- WordCount/WordCount/word-counter.js
+++ WordCount/WordCount/word-counter.js
@@ -1,6 +1,27 @@
-let $P = document.querySelector("p");
-let $TEXT = $P.innerText;
-let $LIST = $TEXT.split(" ");
-let $WORDS = $LIST.length;
-let $TIME = $WORDS / 250;
-$P.innerText += " (Reading Time: " + $TIME + " minutes)";
+// 1. Find the first paragraph 
+// 2. Count the number of words in it 
+// 3. Calculate how long it would take to read 
+//    Avg reading speed in US: ~250 words per minute 
+// 4. update the paragraph to include the reading time 
+function countWords (the_text) {
+    return the_text.split(" ").length;
+}
+function wordsToMinutes(words) {
+    return words / 250;
+}
+function insertReadingTime($p) {
+  if ($p.innerText && $p.matches("p")) {
+    let words = countWords($p.innerText);
+    let duration = wordsToMinutes(words);
+    $p.innerText += " (Reading time: " + duration + " minutes)";
+
+  }
+}
+function insertReadingTimeForAllParagraphs() {
+    let $paragraphs = document.querySelectorAll("p");
+    let count = $paragraphs.length;
+    for (let i = 0; i < count; i++) {
+        insertReadingTime($paragraphs[i]);
+    }
+}
+insertReadingTimeForAllParagraphs();
