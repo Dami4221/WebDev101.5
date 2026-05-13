a4df68d217940ca043e177b7495e28b862b755e3
Demi Peal <pamiendeal@gmail.com>
2026-05-13 10:52:37 -0500

    file fix

--- functional/word-counter.js
+++ functional/word-counter.js
@@ -3,18 +3,25 @@
 // 3. Calculate how long it would take to read 
 //    Avg reading speed in US: ~250 words per minute 
 // 4. update the paragraph to include the reading time 
-function insertReadingTime($p) {
-  if ($p.innerText && $p.matches("p")) {
-  let $p = document.querySelector("p");
-  let words = countWords($p.innerText);
-  let duration = wordsToMinutes(words);
-  $p.innerText += " (Reading time: " + duration + " minutes)";
-}
-insertReadingTime();
-
-function countWords(the_text) {
+function countWords (the_text) {
     return the_text.split(" ").length;
 }
 function wordsToMinutes(words) {
     return words / 250;
 }
+function insertReadingTime($p) {
+  if ($p.innerText && $p.matches("p")) {
+    let words = countWords($p.innerText);
+    let duration = wordsToMinutes(words);
+    $p.innerText += " (Reading time: " + duration + " minutes)";
+
+  }
+}
+function insertReadingTimeForAllParagraphs() {
+    let $paragraphs = document.querySelectorAll("p");
+    let count = $paragraphs.length;
+    for (let i = 0; i < count; i++) {
+        insertReadingTime($paragraphs[i]);
+    }
+}
+insertReadingTimeForAllParagraphs();
